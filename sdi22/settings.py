077b38cd1cb283b31b95fb784e343627a0f818a9
SDILogin <DISelivanov@gmail.com>
2015-10-17 00:18:14 +0300

    db and simple note model added

--- sdi22/settings.py
+++ sdi22/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'sdi22.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='postgres://kkvtkxgcdowmpb:XXtmaQRr-PDjxM1kkmWaZtZyJ0@ec2-54-83-29-15.compute-1.amazonaws.com:5432/donkp0gsgvdmq')
 }
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 
 # Internationalization
